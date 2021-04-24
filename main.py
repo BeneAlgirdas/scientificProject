b9fd200d1b701ebe85adfbea75346d6a3d421c24
Algirdas Benetis <Algirdas.Benetis@Gmail.com>
2021-04-24 19:37:23 +0300

    Reading all images, multiple lines in label text file read

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import time
+
 import torch
 import torchvision.models
+from torch import optim
 from torch.utils import data
 import os
 from PIL import Image
-import numpy as np
 import argparse
-from matplotlib.pyplot import imshow
+from torch.utils.data import DataLoader
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision import ops
 
@@ -21,38 +23,21 @@ class ImageDataset(data.Dataset):
                 on a sample.
         """
         self.root_dir = root_dir
-        self.result = []
-        for _, _, filenames in os.walk(root_dir):
-            for f in filenames:
-                if f.endswith('.jpg'):
-                    self.result.append(f)
 
-        print(self.result)
-
-        # Read image
-        label = 'human'
-        pil_image = Image.open(os.path.join(self.root_dir, label, self.result[0]))
-        print(self.root_dir)
-        print(label)
-        print(self.result[0])
-        print(np.asarray(pil_image))
-        print(pil_image)
-
-        with open(os.path.join(self.root_dir, label, 'label', self.result[0].replace('.jpg', '.txt'))) as f:
-            read_data = f.read()
-            print(read_data)
-            coordinates_list = (read_data.split(' ', 1))
-            coordinates_list = coordinates_list[1].split(' ')
-            coordinates = float(coordinates_list[0]), float(coordinates_list[1]), float(coordinates_list[2]), float(
-                coordinates_list[3])
-            print(coordinates)
-
-        # self.landmarks_frame = pd.read_csv(csv_file)
-        # self.root_dir = root_dir
-        # self.transform = transform
+        self.image_and_label_index_dictionary = {}
+        self.index_and_label_dictionary = {}
+        self.filenames = []
+        for i, dir_label in enumerate(os.listdir(root_dir)):
+            current_files = os.listdir(os.path.join(root_dir, os.listdir(root_dir)[i]))
+            current_files.remove('label')
+            self.filenames.extend(current_files)
+            self.index_and_label_dictionary[i] = dir_label
+            for filename in current_files:
+                self.image_and_label_index_dictionary[filename] = i
+        # print(len(self.image_and_label_index_dictionary))
 
     def __len__(self):
-        return len(self.result)
+        return len(self.filenames)
 
     def __getitem__(self, idx):
         # use iterator over image names
@@ -60,18 +45,58 @@ class ImageDataset(data.Dataset):
         #                         self.landmarks_frame.iloc[idx, 0])
         # image = io.imread(img_name)
         # Read image
-        label = 'human'
-        pil_image = Image.open(os.path.join(self.root_dir, label, self.result[0]))
-        print(np.asarray(pil_image))
-        print(pil_image)
-        target = (label, (12, 65, 12, 64))
+        # print(self.index_and_label_dictionary)
+        # print(self.image_and_label_index_dictionary)
+        # print(self.filenames[idx])
+        label = self.index_and_label_dictionary[self.image_and_label_index_dictionary[self.filenames[idx]]]
+        # print('label' + label)
+        # print('path' + os.path.join(self.root_dir, label, self.filenames[idx]))
+        pil_image = Image.open(os.path.join(self.root_dir, label, self.filenames[idx]))
+
+        with open(os.path.join(self.root_dir, label, 'label', self.filenames[idx].replace('.jpg', '.txt'))) as f:
+            read_data = f.readlines()
+            labels = []
+            coordinates = []
+            target = {}
+            for line in read_data:
+                label_arguments = line.split(' ', 1)
+                labels.append(list(self.index_and_label_dictionary.keys())
+                              [list(self.index_and_label_dictionary.values()).index(label_arguments[0].lower())])
+                coordinates_string = label_arguments[1].split(' ')
+                coordinates_tuple = float(coordinates_string[0]), float(coordinates_string[1]), float(
+                    coordinates_string[2]), float(
+                    coordinates_string[3])
+                coordinates.append(torch.FloatTensor(coordinates_tuple))
+
+        target['labels'] = labels
+        target['coordinates'] = coordinates
         return pil_image, target
 
 
+cs = ImageDataset('./data/train')
+
+
+def default_collate(batch):
+    return batch
+
+
+dataloader = DataLoader(cs, batch_size=256, shuffle=False, collate_fn=default_collate)
+
+before = time.time()
+for i, batch in enumerate(dataloader):
+    print(len(batch))
+e = time.time() - before
+print(e)
 # folder = torchvision.datasets.ImageFolder(root='./data/train')
 # loader = data.DataLoader(folder)
 
-cs = ImageDataset('./data/train')
+
+print(cs.__getitem__(0))
+print(cs.__getitem__(1))
+print(cs.__getitem__(2))
+print(cs.__getitem__(3))
+print(cs.__getitem__(3609))
+print(cs.__getitem__(6250))
 
 backbone = torchvision.models.resnet50(True)
 backbone.out_channels = 1280
@@ -82,12 +107,15 @@ parser.add_argument('--batch_size', type=int, default=256)
 parser.add_argument('--num_classes', type=int, default=3)
 
 args = parser.parse_args()
-print(args.batch_size)
 
-box_roi_pool = ops.MultiScaleRoIAlign(['0'], ['14'], ['2'])
+box_roi_pool = ops.MultiScaleRoIAlign(['0'], 14, 2)
 rpn_anchor_generator = AnchorGenerator()
 model = torchvision.models.detection.FasterRCNN(backbone=backbone, num_classes=args.num_classes,
                                                 rpn_anchor_generator=rpn_anchor_generator, box_roi_pool=box_roi_pool)
 
+# TODO: Learning rate is linearly increased from
+# 0 to 0.16 in the first training epoch and then annealed down
+# using cosine decay rule.
+optimizer = optim.SGD(model.parameters(), lr=0.16, momentum=0.9, weight_decay=4e-5)
 
 cuda = torch.device('cuda')
